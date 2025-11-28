28c9e02b655e7d9078fc68a997373e826cf22986
Thales Oliveira <thales930@gmail.com>
2025-11-27 23:15:39 -0300

    Implementando movimentos possíveis da Torre

--- src/xadrez/PecasXadrez.java
+++ src/xadrez/PecasXadrez.java
@@ -2,6 +2,7 @@ package xadrez;
 
 import tabuleiro.Mesa;
 import tabuleiro.Peca;
+import tabuleiro.Posicao;
 
 public abstract class PecasXadrez extends Peca {
 
@@ -16,9 +17,17 @@ public abstract class PecasXadrez extends Peca {
 		return cor;
 	}
 
-	//Pois não haverá permissão para trocar a cor da peça seja modificada. Somente acessada
+	// Pois não haverá permissão para trocar a cor da peça seja modificada. Somente
+	// acessada
 //	public void setCor(Cor cor) {
 //		this.cor = cor;
 //	}
 
+	protected boolean existeAlgumaPeçaOponente(Posicao posicao) {
+
+		// Foi realizado um downcast para poder funcionar
+		// (PecasXadrez)getMesa().peca(posicao)
+		PecasXadrez p = (PecasXadrez) getMesa().peca(posicao);
+		return p != null && p.getCor() !=  cor;
+	}
 }
--- src/xadrez/pecas/Torre.java
+++ src/xadrez/pecas/Torre.java
@@ -1,6 +1,7 @@
 package xadrez.pecas;
 
 import tabuleiro.Mesa;
+import tabuleiro.Posicao;
 import xadrez.Cor;
 import xadrez.PecasXadrez;
 
@@ -19,6 +20,56 @@ public class Torre extends PecasXadrez {
 	@Override
 	public boolean[][] possiveisMovimentos() {
 		boolean[][] mat = new boolean[getMesa().getLinhas()][getMesa().getColunas()];
+
+		Posicao p = new Posicao(0, 0);
+
+		// Acima
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+		while (getMesa().posicaoExistente(p) && !getMesa().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+
+		if (getMesa().posicaoExistente(p) && existeAlgumaPeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Esquerda
+		p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		while (getMesa().posicaoExistente(p) && !getMesa().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+			;
+		}
+
+		if (getMesa().posicaoExistente(p) && existeAlgumaPeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Direita
+		p.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+		while (getMesa().posicaoExistente(p) && !getMesa().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() + 1);
+			;
+		}
+
+		if (getMesa().posicaoExistente(p) && existeAlgumaPeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// Para baixo
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		while (getMesa().posicaoExistente(p) && !getMesa().temUmaPeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+
+		if (getMesa().posicaoExistente(p) && existeAlgumaPeçaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
 		return mat;
 	}
+
 }
